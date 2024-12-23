b454ebcf27cbaa6723ad1f0ddb0dd3028333a04f
RihamRamzi <rihamsoftware@gmail.com>
2024-12-23 22:15:54 +0530

    created add task features and styled it to submit the value

--- src/index.js
+++ src/index.js
@@ -2,10 +2,27 @@ import "./styles.css";
 
 const addTask = document.querySelector("#addTask");
 const dialog = document.querySelector("#taskDialog");
+const closeTask = document.querySelector("#closeBtn");
+const submitBtn = document.querySelector("#submitBtn");
+const form = document.querySelector("form");
+
+// input
+const task = document.querySelector("#task");
+const description = document.querySelector("#des");
+const date = document.querySelector("#date");
+
 addTask.addEventListener("click", () => {
   dialog.showModal();
 });
 
-closeButton.addEventListener("click", () => {
+closeTask.addEventListener("click", (event) => {
+  event.preventDefault();
+  form.reset();
+  dialog.close();
+});
+
+submitBtn.addEventListener("click", (event) => {
+  event.preventDefault();
+  form.reset();
   dialog.close();
 });
