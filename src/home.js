3570f41c713257e2ed32d0bdb17f94b8f84194c6
RihamRamzi <rihamsoftware@gmail.com>
2025-01-06 17:11:15 +0530

    made the code simple by making a separate function which creates the dom display elements
    we can use that func and pass the arguments
    eg; like title des and etc then it creates the
    element for us

--- src/home.js
+++ src/home.js
@@ -1,6 +1,7 @@
 import { projects, clearProjectSelect } from "./createProject";
 import editSvg from "./svg/square-edit-outline.svg";
 import deleteSvg from "./svg/delete.svg";
+import { createTaskDom } from "./addTask";
 
 const allTasks = (event) => {
   const clickedElement = event.target.closest("#allTasks");
@@ -20,51 +21,7 @@ const displayAllTasksDom = () => {
   tasksContainer.textContent = "";
   projects.forEach((project) => {
     project.task.forEach((task) => {
-      const tasksConDom = document.querySelector(".tasksContainer");
-
-      const taskCon = document.createElement("div");
-      taskCon.className = "task";
-      taskCon.setAttribute("task-id", task.id);
-
-      const checkBox = document.createElement("div");
-      checkBox.className = "checkBox";
-
-      const details = document.createElement("div");
-      details.className = "details";
-
-      const taskTitle = document.createElement("div");
-      const taskDes = document.createElement("div");
-      taskTitle.className = "taskTitle";
-      taskTitle.textContent = task.title;
-
-      taskDes.className = "taskDes";
-      taskDes.textContent = task.description;
-      details.appendChild(taskTitle);
-      details.appendChild(taskDes);
-
-      const datePriority = document.createElement("div");
-      const taskDueDate = document.createElement("div");
-      taskDueDate.className = "taskDueDate";
-      taskDueDate.textContent = task.dueDate;
-      const priority = document.createElement("div");
-      priority.className = "priority";
-      priority.textContent = task.priority;
-      datePriority.appendChild(taskDueDate);
-      datePriority.appendChild(priority);
-
-      const editTask = document.createElement("img");
-      editTask.className = "taskSvg editTaskSvg";
-      editTask.src = editSvg;
-      const deleteTask = document.createElement("img");
-      deleteTask.className = "taskSvg deleteTaskSvg";
-      deleteTask.src = deleteSvg;
-
-      taskCon.appendChild(checkBox);
-      taskCon.appendChild(details);
-      taskCon.appendChild(datePriority);
-      taskCon.appendChild(editTask);
-      taskCon.appendChild(deleteTask);
-      tasksConDom.appendChild(taskCon);
+      createTaskDom(task.id, task.title, task.description, task.priority);
     });
   });
   const addTask = document.querySelector("#addTask");
